e7f8b5dc91bc16cec2a1dd6c9c3d2b9ee5d39a25
muinocriz <gongchao1210@gmail.com>
2018-03-31 01:01:12 +0800

    finished Customer service login front end Ref #19

---
+++ HFUT_Group3(Front-end)/cs-vue-project/src/components/CsLogin.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="6" :offset="9">
+      <div class="grid-content subrow">
+        <el-form :model="csLoginForm" status-icon ref="csLoginForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="邮箱" prop="cs_email" :rules="[
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
+    ]">
+            <el-input v-model="csLoginForm.cs_email"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="cs_pwd" :rules="{
+      required: true, message: '密码不能为空', trigger: 'blur'
+    }">
+            <el-input type="password" v-model="csLoginForm.cs_pwd" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="subbtn" type="primary" @click="submitForm('csLoginForm')">提交</el-button>
+            <el-button class="subbtn" @click="resetForm('csLoginForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      csLoginForm: {
+        cs_email: '',
+        cs_pwd: ''
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style>
+.subbtn {
+  width: 45%;
+}
+.subrow {
+  margin-top: 20%;
+}
+</style>
--- HFUT_Group3(Front-end)/cs-vue-project/src/router/index.js
+++ HFUT_Group3(Front-end)/cs-vue-project/src/router/index.js
@@ -2,15 +2,89 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import CSM_UI from '@/components/CSM_UI'
+import CsLogin from '@/components/CsLogin'
+import CsRegister from '@/components/CsRegister'
+import AdminHome from '@/components/AdminHome'
+import index from '@/components/index'
+import kfgl from '@/components/KfManage'
+import yhgl from '@/components/CsManage'
+import settings from '@/components/Settings'
+import know from '@/components/Know'
+import UserChat from '@/components/UserChat'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'CSM_UI',
       component: CSM_UI
+    },
+    {
+      path: '/UserChat',
+      name: '聊天',
+      component: UserChat
+    },
+    {
+      path: '/CsLogin',
+      name: '客服登录',
+      component: CsLogin
+    },
+    {
+      path: '/CsRegister',
+      name: '客服注册',
+      component: CsRegister
+    },
+    {
+      path: '/',
+      name: 'AdminHome',
+      redirect: '/index',
+      component: AdminHome,
+      children: [{
+        path: '/index',
+        component: index,
+        name: '首页'
+      }]
+    },
+    {
+      path: '/',
+      name: '客服管理',
+      component: AdminHome,
+      children: [{
+        path: '/kfgl',
+        component: kfgl,
+        name: '客服管理'
+      }]
+    },
+    {
+      path: '/',
+      name: '用户管理',
+      component: AdminHome,
+      children: [{
+        path: '/yhgl',
+        component: yhgl,
+        name: '用户管理'
+      }]
+    },
+    {
+      path: '/',
+      name: '设置',
+      component: AdminHome,
+      children: [{
+        path: '/settings',
+        component: settings,
+        name: '设置'
+      }]
+    },
+    {
+      path: '/',
+      name: '知识库',
+      component: AdminHome,
+      children: [{
+        path: '/know',
+        component: know,
+        name: '知识库'
+      }]
     }
   ]
 })
