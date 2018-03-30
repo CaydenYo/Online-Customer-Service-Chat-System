6be9370288166adfc1a8dbe7ef84de593e63d378
muinocriz <gongchao1210@gmail.com>
2018-03-31 01:09:41 +0800

    finished Customer service manager page frame Ref #23

---
+++ HFUT_Group3(Front-end)/cs-vue-project/src/components/CsManage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="yhgl">
+    <el-card class="box-card">
+      <h3>用户列表</h3>
+      <el-table :data="tableData3" border style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址">
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData3: [
+        {
+          date: '2016-05-03',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-02',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-04',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-01',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-08',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-06',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        },
+        {
+          date: '2016-05-07',
+          name: '张晓明',
+          address: '合肥翡翠湖6#'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ HFUT_Group3(Front-end)/cs-vue-project/src/components/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="allActive">
+    <el-row class="head">
+      <el-col :span="24" style="padding:20px 0;background: #33CCCC;">
+        <el-col :span="6">
+          {{ totalKfNum }}<br>
+          <span>客服总数</span>
+        </el-col>
+        <el-col :span="6">
+          {{ totalActiveNum }}<br>
+          <span>在线客服</span>
+        </el-col>
+        <el-col :span="6">
+          {{ inConv }}<br>
+          <span>接入会话</span>
+        </el-col>
+        <el-col :span="6">
+          {{ inWait }}<br>
+          <span>正在等待</span>
+        </el-col>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'allActive',
+  data: function() {
+    return {
+      totalKfNum: 10,
+      totalActiveNum: 5,
+      inConv: 25,
+      inWait: 28
+    }
+  }
+}
+</script>
+
+
+<style>
+.allActive > .head > .el-col > .el-col {
+  padding: 20px 0;
+  border-right: solid 1px #fff;
+}
+
+.allActive .head {
+  text-align: center;
+  color: #fff;
+  font-size: 30px;
+  margin-bottom: 50px;
+}
+
+.allActive .head span {
+  font-size: 16px;
+}
+</style>
