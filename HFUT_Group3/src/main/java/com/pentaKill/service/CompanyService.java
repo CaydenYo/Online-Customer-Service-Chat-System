2850837a5a51b18129ba4b0cdee10c37838c1f18
hyc <841160480@qq.com>
2018-04-02 11:18:27 +0800

    Finish the company settings.Ref#46

---
+++ HFUT_Group3/src/main/java/com/pentaKill/service/CompanyService.java
@@ -0,0 +1,39 @@
+package com.pentaKill.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.pentaKill.dao.CompanyMapper;
+import com.pentaKill.domain.Company;
+import com.pentaKill.domain.CustomerService;
+@Service
+public class CompanyService {
+    
+    @Resource
+    CompanyMapper companyMapper;
+    
+    
+    public Company findCompany(int company_id) {
+        return companyMapper.findCompany(company_id);
+    }
+    
+
+    public void update(Company company, int dstribution_type, int customer_info_flag, int min_num) {
+           company.setMininum_operating_num(min_num);
+           company.setDstribution_type(dstribution_type);
+           company.setCustomer_info_flag(customer_info_flag);
+           companyMapper.UpdateCompany(company);
+    }
+    
+    public CustomerService selectCustomerService(Company company){
+        int company_id = company.getCompany_id();
+        int min_num = company.getMininum_operating_num();
+        return companyMapper.selectCustomerService(company_id, min_num);
+    }
+    
+    public void updateCustomerService(CustomerService customerService,int min_num){
+        int cs_id = customerService.getCs_id();
+        companyMapper.updateCustomerService(min_num, cs_id);
+    }
+}
