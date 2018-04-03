6f3286da34e2be7c18312bbe86b12368326e5e69
muinocriz <gongchao1210@gmail.com>
2018-04-03 22:40:06 +0800

    Finish customer service suspension chat box Ref #53

---
+++ HFUT_Group3(Front-end)/xfChat/src/page/chat/chat.vue
@@ -0,0 +1,45 @@
+<template>
+<div id="hehe" >
+        <div id="xf" style="background-color:red;" v-if="show">
+            <div class="content" style="background-color:blue;">
+                <div class="chatbox" style="background-color:green;">
+                    <message></message>
+                    <v-text></v-text>
+                </div>
+            </div>
+        </div>
+        <button @click="show=!show" style="position: fixed; right: 5px; bottom: 60px; z-index: 10; padding: 3px 3px 0 3px;">咨詢</button></div>
+</template>
+
+<script>
+import message from '../../components/message/message'
+import vText from '../../components/text/text'
+export default {
+    data(){
+        return{
+            show:true
+        }
+    },
+  components: {
+    message,
+    vText
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.content {
+    display: flex;
+    width: 500px;
+}
+
+.chatbox {
+    flex: 1;
+}
+
+.xf {
+    position: absolute;
+    z-index: 99999;
+    margin-top: 5%;
+}
+</style>
