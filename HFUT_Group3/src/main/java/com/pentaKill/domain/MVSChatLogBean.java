d8b12cd17410dea19b0ac79d845ebea0d9480326
yukang <809049664@qq.com>
2018-04-11 14:12:08 +0800

    Finish the background administrator looks at the current session and chat record. Ref #69

---
+++ HFUT_Group3/src/main/java/com/pentaKill/domain/MVSChatLogBean.java
@@ -0,0 +1,115 @@
+package com.pentaKill.domain;
+
+import java.sql.Timestamp;
+
+public class MVSChatLogBean {
+    private int conversation_id;
+    private int receiver_id;
+    private String receiver_nickname;
+    private int sender_id;
+    private String sender_nickname;
+    private int from_customer;
+    private Timestamp time;
+    private int content_type;
+    private String content;
+
+    public MVSChatLogBean() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public MVSChatLogBean(int conversation_id, int receiver_id, String receiver_nickname, int sender_id,
+            String sender_nickname, int from_customer, Timestamp time, int content_type, String content) {
+        super();
+        this.conversation_id = conversation_id;
+        this.receiver_id = receiver_id;
+        this.receiver_nickname = receiver_nickname;
+        this.sender_id = sender_id;
+        this.sender_nickname = sender_nickname;
+        this.from_customer = from_customer;
+        this.time = time;
+        this.content_type = content_type;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "MVSChatLogBean [conversation_id=" + conversation_id + ", receiver_id=" + receiver_id
+                + ", receiver_nickname=" + receiver_nickname + ", sender_id=" + sender_id + ", sender_nickname="
+                + sender_nickname + ", from_customer=" + from_customer + ", time=" + time + ", content_type="
+                + content_type + ", content=" + content + "]";
+    }
+
+    public int getConversation_id() {
+        return conversation_id;
+    }
+
+    public void setConversation_id(int conversation_id) {
+        this.conversation_id = conversation_id;
+    }
+
+    public int getReceiver_id() {
+        return receiver_id;
+    }
+
+    public void setReceiver_id(int receiver_id) {
+        this.receiver_id = receiver_id;
+    }
+
+    public String getReceiver_nickname() {
+        return receiver_nickname;
+    }
+
+    public void setReceiver_nickname(String receiver_nickname) {
+        this.receiver_nickname = receiver_nickname;
+    }
+
+    public int getSender_id() {
+        return sender_id;
+    }
+
+    public void setSender_id(int sender_id) {
+        this.sender_id = sender_id;
+    }
+
+    public String getSender_nickname() {
+        return sender_nickname;
+    }
+
+    public void setSender_nickname(String sender_nickname) {
+        this.sender_nickname = sender_nickname;
+    }
+
+    public int getFrom_customer() {
+        return from_customer;
+    }
+
+    public void setFrom_customer(int from_customer) {
+        this.from_customer = from_customer;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public int getContent_type() {
+        return content_type;
+    }
+
+    public void setContent_type(int content_type) {
+        this.content_type = content_type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
