b278ac531c75029b1865f1cdda76e4eab8d45bf3
叶飞 <846953477@qq.com>
2018-04-13 21:18:10 +0800

    Complete the function modification of Websocket. Ref #77

--- HFUT_Group3/src/main/java/com/pentaKill/dao/SessionTransferMapper.java
+++ HFUT_Group3/src/main/java/com/pentaKill/dao/SessionTransferMapper.java
@@ -3,9 +3,9 @@ package com.pentaKill.dao;
 import com.pentaKill.domain.ConversationBean;
 
 public interface SessionTransferMapper {
-    public void decreaseCsWaitingNumMapper(int cs_id);
+    public void decreaseCsOperatedNumMapper(int cs_id);
 
-    public void addCsWaitingNumMapper(int cs_id);
+    public void addCsOperatedNumMapper(int cs_id);
 
     public void closeConversationMapper(ConversationBean cb);
 
